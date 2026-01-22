4b934dc1ced57b1c1d303f4fe73ac0e09311ef46
tduncan14 <nbatre1@yahoo.com>
2026-01-22 13:15:50 -0600

    keep moving forward

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { BrowserRouter } from 'react-router'
+import { Toaster } from "react-hot-toast";
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+      <Toaster />
+
+    </BrowserRouter>
+
   </StrictMode>,
 )
